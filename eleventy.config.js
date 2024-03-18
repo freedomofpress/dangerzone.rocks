19e27e1185488797cae3047526190b9ab4726c72
Harris Lapiroff <harris@freedom.press>
2024-03-18 16:40:21 -0400

    Make an initial attempt at an 11ty powered dangerzone.rocks

---
+++ eleventy.config.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets")
+    eleventyConfig.addPassthroughCopy("src/checksums.txt")
+    eleventyConfig.addPassthroughCopy("src/favicon.ico")
+
+    eleventyConfig.addCollection("posts", function(collection) {
+        return collection.getFilteredByGlob("src/posts/*.md");
+    })
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            includes: '_layouts',
+        }
+    }
+}
---
+++ src/README.md
@@ -0,0 +1,6 @@
+# Dangerzone.rocks website
+
+This is a static website, currently hosted at https://dangerzone.rocks/
+
+You can edit it locally and test it in your browser without the use of a
+webserver or a static site generator.

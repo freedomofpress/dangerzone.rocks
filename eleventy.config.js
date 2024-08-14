c93b86b842f33a4ae6a61a4a8d6210212dfe7965
Erik Moeller <erik@freedom.press>
2024-08-13 17:19:28 -0700

    Add a basic blog and RSS feed
    Does not yet cover archival of old posts. To add a post,
    simply create it in src/news in the <ISODATE>-title.md format,
    and rebuild.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,27 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets")
-    eleventyConfig.addPassthroughCopy("src/favicon.ico")
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPlugin(pluginRss);
 
     eleventyConfig.addCollection("posts", function(collection) {
-        return collection.getFilteredByGlob("src/posts/*.md");
-    })
+        return collection.getFilteredByGlob("src/news/*.md");
+    });
+
+    eleventyConfig.addFilter("dateIso", date => {
+        return date.toISOString();
+    });
+
+    eleventyConfig.addFilter("dateReadable", date => {
+       return date.toDateString();
+    });
 
     return {
         dir: {
             input: "src",
             output: "dist",
-            includes: '_layouts',
+            includes: "_layouts"
         }
-    }
+    };
 }
